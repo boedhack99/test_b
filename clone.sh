a5066c99988eaae98ce76337ad9db37484cc274e
NAME <mail@gmail.com>
2021-05-01 20:59:13 +0700

    first

---
+++ clone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# write commands if you want to clone some repos
+git clone https://github.com/PixelExperience-Devices/device_xiaomi_mojito -b eleven device/xiaomi/mojito
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_mojito -b eleven vendor/xiaomi/mojito
+git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_mojito -b eleven kernel/xiaomi/mojito
+git clone https://github.com/crdroidandroid/android_device_crdroid_sepolicy -b 11.0 device/crdroid/sepolicy
---
+++ config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GITHUB_USER=boedhack99
+GITHUB_EMAIL=boedhack@gmail.com
+
+KBUILD_BUILD_USER="alvi"
+KBUILD_BUILD_HOST="dblenk9"
+
+export oem=xiaomi
+export device=redmi_mojito
+
+ROM="cRDroid"
+manifest_url="https://github.com/crdroidandroid/android"
+export rom_vendor_name="aosp"
+branch="android-11.0"
+
+release_repo="boedhack99/test_builds"
+
+timezone="Asia/Jakarta"
